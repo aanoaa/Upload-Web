61ea08c508814741c6a04cfe7d55309c1409478b
Yuni Kim <unicozy@gmail.com>
2011-07-04 18:06:13 +0900

    Rewrite script/init_db.sh

--- script/init_db.sh
+++ script/init_db.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
+DB_SQL=db/init.sql
 DB_FILE=db/upload.db
 
-if [ -e ./db ]; then
-	echo Creating $DB_FILE...
-
-	if [ -e $DB_FILE ]; then
-		echo $DB_FILE is already exists.
-	else
-		sqlite3 $DB_FILE <<__SQL__
-	CREATE TABLE upload (
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		md5 TEXT NOT NULL,
-		fname TEXT NOT NULL,
-		download INTEGER NOT NULL DEFAULT 0
-	);
-__SQL__
-		echo Done.
-	fi
-else
+if [ -e $DB_FILE ]; then
+	echo $DB_FILE is already exists.
 	echo Usage : script/init_db.sh
+	exit
 fi
+
+if [ ! -e $DB_SQL ]; then
+	echo $DB_SQL is not exists.
+	echo Usage : script/init_db.sh
+	exit
+fi
+
+echo Creating $DB_FILE...
+cat $DB_SQL | sqlite3 $DB_FILE
+echo Done.
